8966b501300c620cf869e7f888a4660a360ed1e6
Michelle Moon <robbins.michelle@ymail.com>
2020-09-26 11:58:44 -0500

    working on html and timer in js

--- source/script.js
+++ source/script.js
@@ -1,44 +1,77 @@
-var something =document.querySelector("container");
+var something = document.querySelector("container");
 var questionEl = document.querySelector("intial");
 var conentEl = document.querySelector("info");
 var bttnEl = document.querySelector("start-game");
+// total correcr variable for score? total++ correct answers/total-- will trigger time reduction
 
-var countdownEl = "";
+var countdownEl = document.querySelector("time-score");
 var time = "";
-var questionIndex =0;
-var questions = [
+var questionIndex = 0;
+var quiz = [
     {
-        question: "6+6",
+        question1: "The condition in an if/else statement is enclosed with_____",
         answers: [
-            12,30,9,0,],
-        correct: 12
+            "quotes", "curly brackets", "parentheses", "square brackets"],
+        correct: "parentheses"
 
     },
     {
-        question: "10+6",
+        question2: "Which of the following is true: Arrays can be used to store:",
         answers: [
-            16,30,9
-    
+            "numbers and strings", "other arrays", "booleans", "all of the above"
+
         ],
-        correct: 16
+        correct: "all of the above"
 
     },
-]
+    {
+        question3: "What do we enclose string values in?",
+        answers: ["commas", "curly brackets", "quotes", "parentheses"],
+        correct: "quotes"
+
+
+    },
+    {
+        question4: "What is a useful tool for debugging?",
+        answers: ["JavaScript", "terminal", "for loops", "console.log"],
+        correct: "console.log"
+    },
+
+];
+// addeventlistener start on click
+// prev.addeventlistner("click" function(event){
+// event.stopPropagation();}); 
+
 // on click function
 // if we click the button timer begins and 1st question starts
 // what is my timer doing?
-var time = 10;
-countdownEl.textContent = time + "Seconds left";
-var andrew = setInterval(function()
-{
-    time--;
-    if (time ===0){
-        // clearInterval(andrew); function shows highscore
-        textContent = "";
-        alert ("score");
-    }
-    }, 10000)
+bttnEl.addEventListener("click", function (event) {
+    if ()
+})
+
+
+
+function scoreCounter() {
+    var time = 100;
+    var score = setInterval(function () {
+        time--;
+        if (time === 0) {
+            // clearInterval(andrew); function shows highscore
+            countdownEl.textContent = "";
+            tallyScore();
+            clearInterval(time);
+        }, 1000)
+}
 };
+function tallyScore (){
+    if()    {
+
+    }
+    else{
+
+    }
+
+}
 
 
 
@@ -46,14 +79,12 @@ var andrew = setInterval(function()
 
 
 
- var questionDisplay = document.getElementById("question-answer");
- questionDisplay.innerHTML=(questions[questionIndex].question);
-console.log(questions[0]);
-console.log(questions.question);
-console.log(questions[0].question);
+// var questionDisplay = document.querySelector("start-game");
+// questionDisplay.innerHTML = (quiz[questionIndex].question1);
+// console.log(questions[0].question);
 
-var next = document.getElementById("next-button");
-next.addEventListener("click", function(event){
-    questionIndex++;
-    questionDisplay.innerHTML = (questions[questionIndex].question);
-});
+// var next = document.getElementById("next-button");
+// next.addEventListener("click", function (event) {
+    // questionIndex++;
+    // questionDisplay.innerHTML = (questions[questionIndex].question);
+// });
