a01de89836f4426aa1161ee485ccc70d0ba0cfa7
Michelle Moon <robbins.michelle@ymail.com>
2020-09-25 20:23:34 -0500

    intial html and assign js var

--- source/script.js
+++ source/script.js
@@ -1,11 +1,16 @@
+var something =document.querySelector("container");
+var questionEl = document.querySelector("intial");
+var conentEl = document.querySelector("info");
+var bttnEl = document.querySelector("start-game");
+
+var countdownEl = "";
+var time = "";
 var questionIndex =0;
 var questions = [
     {
         question: "6+6",
         answers: [
-            12,30,9
-    
-        ],
+            12,30,9,0,],
         correct: 12
 
     },
@@ -18,7 +23,29 @@ var questions = [
         correct: 16
 
     },
-];
+]
+// on click function
+// if we click the button timer begins and 1st question starts
+// what is my timer doing?
+var time = 10;
+countdownEl.textContent = time + "Seconds left";
+var andrew = setInterval(function()
+{
+    time--;
+    if (time ===0){
+        // clearInterval(andrew); function shows highscore
+        textContent = "";
+        alert ("score");
+    }
+    }, 10000)
+};
+
+
+
+
+
+
+
  var questionDisplay = document.getElementById("question-answer");
  questionDisplay.innerHTML=(questions[questionIndex].question);
 console.log(questions[0]);
